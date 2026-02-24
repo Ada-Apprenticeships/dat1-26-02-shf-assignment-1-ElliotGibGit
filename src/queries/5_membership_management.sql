a537cfdd7ada15886c27ed9df40330d5117c37b1
ElliotGibGit <elliot.gibson@ada.ac.uk>
2026-02-24 17:43:29 +0000

    5 done

--- src/queries/5_membership_management.sql
+++ src/queries/5_membership_management.sql
@@ -2,10 +2,22 @@
 .mode column
 
 -- 5.1 
+SELECT m.member_id,m.first_name,m.last_name,ms.type as membership_type,m.join_date
+FROM MEMBERS m
+JOIN memberships ms ON m.member_id = ms.member_id
+WHERE ms.status='Active';
 
 
 -- 5.2 
+SELECT ms.type AS membership_type,
+ROUND(AVG((strftime('%s', a.check_out_time) - strftime('%s', a.check_in_time)) / 60.0), 2) AS avg_visit_duration_minutes
+FROM attendance a
+JOIN memberships ms ON a.member_id = ms.member_id
+GROUP BY ms.type;
 
 
 -- 5.3 
-
+SELECT m.member_id,m.first_name,m.last_name,m.email,ms.end_date
+FROM MEMBERS m
+JOIN memberships ms ON m.member_id = ms.member_id
+WHERE ms.end_date BETWEEN '2025-01-01' AND '2025-12-31';
